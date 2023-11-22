952dc13a7611e1863318c08cd3c57cc4c284974e
codeCatCode <110313547+donow1@users.noreply.github.com>
2023-11-22 11:58:03 +0800

    Update AHeader.jsx

--- src/components/AHeader.jsx
+++ src/components/AHeader.jsx
@@ -25,9 +25,9 @@ export default function AHeader() {
                     <a href='/#DTextResources' class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#dea6a0] md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Learning Tips</a>
                 </li>
 
-                <li>
+{/*                 <li>
                     <a href="/#ESignUpOrLogin" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#dea6a0] md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Join Events</a>
-                </li>
+                </li> */}
 
                 <li>
                     <a href="/#FContactMe" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#dea6a0] md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact Me</a>
